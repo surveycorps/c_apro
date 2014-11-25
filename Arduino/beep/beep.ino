f60def60c2b29b2501de96009dcf506778693423
muniza <muniza@seas.upenn.edu>
2014-11-25 18:58:06 -0500

    added example code for core control

---
+++ Arduino/beep/beep.ino
@@ -0,0 +1,50 @@
+int led = 15; // E1
+int M1 = 18; // D0
+int M2 = 19; // D1
+int M3 = 14; // E0
+int dir1 = A0; // A1
+int dir2 = A1; // A2
+int dir3 = A2; // A3
+//int M1L = C0; // C0
+int M1R = A3; // A4
+//int M2L = C1; // C1
+int M2R = A4; // A5
+int M3L = 7; // B2
+int M3R = A5; // A6
+
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+  //printf("\nI'm ALIVE\n\r");
+  pinMode(led, OUTPUT);
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(M3, OUTPUT);
+  //pinMode(dir2, OUTPUT);
+  digitalWrite(led, HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    digitalWrite(M3, HIGH);
+    analogWrite(dir1, 255);
+    analogWrite(dir2, 0);
+    analogWrite(dir3, 255);
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    digitalWrite(M3, HIGH);
+    analogWrite(dir1, 0);
+    analogWrite(dir2, 255);
+    analogWrite(dir3, 0);
+    digitalWrite(led, LOW);
+    delay(1000);
+    digitalWrite(M1, LOW);
+    digitalWrite(M2, LOW);
+    digitalWrite(M3, LOW);
+    delay(1000);
+}
